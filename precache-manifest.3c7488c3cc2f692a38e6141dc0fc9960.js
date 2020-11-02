d8382becbd15a4cf6488c5426f657749625df73a
Alexander Ioannidis <a.ioannidis@cern.ch>
2020-11-02 11:11:50 +0100

    Updates

---
+++ precache-manifest.3c7488c3cc2f692a38e6141dc0fc9960.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dccc3dbd44ed088f6849ef9d9d0ec97",
+    "url": "/zenodo-json-validator/index.html"
+  },
+  {
+    "revision": "68cc0f87a16a02e5bf47",
+    "url": "/zenodo-json-validator/static/css/2.e1d0ca00.chunk.css"
+  },
+  {
+    "revision": "430c5167ca058de3eed5",
+    "url": "/zenodo-json-validator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68cc0f87a16a02e5bf47",
+    "url": "/zenodo-json-validator/static/js/2.7843721f.chunk.js"
+  },
+  {
+    "revision": "35d44ebb8460b450c2e17fcf604086b6",
+    "url": "/zenodo-json-validator/static/js/2.7843721f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430c5167ca058de3eed5",
+    "url": "/zenodo-json-validator/static/js/main.05619b79.chunk.js"
+  },
+  {
+    "revision": "9c620aa8d1c985f03d92",
+    "url": "/zenodo-json-validator/static/js/runtime-main.c076ea81.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/zenodo-json-validator/static/media/jsoneditor-icons.2b9b4872.svg"
+  }
+]);
